3728bb4eca0d04243f0ebb5c2039de4569b02eca
frozensteel1 <FrozenSteel@mail.ru>
2020-11-05 23:55:59 +0300

    Установлены авторизация и установлены стартовые страницы немного похимичено с тем куда попадает после отправки

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Мы на главной странице приложения файл main/index
+        </h2>
+    </x-slot>
+
+</x-app-layout>
+
+
+
+
+
